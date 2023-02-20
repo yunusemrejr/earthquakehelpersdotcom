6b9be0fc6d52595f49871c087ab7abff6f1c3aa4
YUNUS EMRE VURGUN <64273622+yunusemrejr@users.noreply.github.com>
2023-02-20 16:56:45 +0300

    Add files via upload

---
+++ turkey/action.php
@@ -0,0 +1,370 @@
+<?php
+ 
+  $chosenCNTRY = isset($_POST['fromCountry']) ? $_POST['fromCountry'] : null;
+  
+ if( $chosenCNTRY !=null){
+ 
+
+
+?>
+
+
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="​Choose Charity Location (choose their origin country for international charities):">
+    <meta name="description" content="">
+            <script type="text/javascript" src="countryfunctions.js"></script>
+<style> table { font-family: arial, sans-serif; border-collapse: collapse; width: 100%; } td, th { border: 1px solid #dddddd; text-align: left; padding: 8px; } tr:nth-child(even) { background-color: darkgray; } </style>
+        <link rel="icon" type="image/x-icon" href="https://www.earthquakehelpers.host/earthquake/images/newicon.ico">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/6.6.6/css/flag-icons.min.css" integrity="sha512-uvXdJud8WaOlQFjlz9B15Yy2Au/bMAvz79F7Xa6OakCl2jvQPdHD0hb3dEqZRdSwG4/sknePXlE7GiarwA/9Wg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Choose Your Location - Earthquake Helpers</title>
+    <link rel="stylesheet" href="https://www.earthquakehelpers.host/earthquake/yunusemrejr.css" media="screen">
+<link rel="stylesheet" href="https://www.earthquakehelpers.host/earthquake/Home.css" media="screen">
+    <script class="u-script" type="text/javascript" src="https://www.earthquakehelpers.host/earthquake/jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="https://www.earthquakehelpers.host/earthquake/yunusemrejr.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.8.2, yunusemrejr.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <style>
+        #h2idchhose{
+            text-align:center !important;
+        }
+        
+        #choose-where-you-from{
+            color:black;
+            
+        }
+        
+        table{
+            
+            table-layout:fixed;
+            
+        }
+    </style>
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "https://www.earthquakehelpers.host/earthquake/images/Untitled_design__6_-removebg-preview.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website">
+  </head>
+  <body data-home-page="https://www.earthquakehelpers.host/earthquake/Home.html" data-home-page-title="Home" class="u-body u-xl-mode"><header class="u-clearfix u-gradient u-header u-header" id="sec-3178"><div class="u-clearfix u-sheet u-sheet-1">
+        <nav class="u-align-right-lg u-align-right-md u-align-right-sm u-align-right-xs u-menu u-menu-one-level u-offcanvas u-menu-1" data-responsive-from="LG">
+          <div class="menu-collapse" style="font-size: 1.25rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-active-color u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-file-icon u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base u-text-white u-file-icon-1" href="https://www.earthquakehelpers.host/">
+              <img src="https://www.earthquakehelpers.host/earthquake/images/1.png" alt="">
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-active-white u-button-style u-nav-link u-text-active-palette-3-base u-text-hover-palette-1-light-2 u-text-white" href="https://www.earthquakehelpers.host/" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-active-white u-button-style u-nav-link u-text-active-palette-3-base u-text-hover-palette-1-light-2 u-text-white" href="https://www.earthquakehelpers.host/earthquake/about-us.html" style="padding: 10px 20px;">About Us</a>
+</li><li class="u-nav-item"><a class="u-active-white u-button-style u-nav-link u-text-active-palette-3-base u-text-hover-palette-1-light-2 u-text-white" href="https://www.earthquakehelpers.host/earthquake/support.html" style="padding: 10px 20px;">Support</a>
+</li><li class="u-nav-item"><a class="u-active-white u-button-style u-nav-link u-text-active-palette-3-base u-text-hover-palette-1-light-2 u-text-white" style="padding: 10px 7px 10px 20px;" href="https://www.earthquakehelpers.host/earthquake/privacy-policy.html">Privacy Policy</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://www.earthquakehelpers.host/">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://www.earthquakehelpers.host/earthquake/about-us.html">About Us</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://www.earthquakehelpers.host/earthquake/support.html">Support</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://www.earthquakehelpers.host/earthquake/privacy-policy.html">Privacy Policy</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <a href="https://www.earthquakehelpers.host/" class="u-image u-logo u-image-1" data-image-width="600" data-image-height="180">
+          <img src="https://www.earthquakehelpers.host/earthquake/images/Untitled_design__6_-removebg-preview.png" class="u-logo-image u-logo-image-1">
+        </a>
+      </div></header>
+    <section class="u-clearfix u-image u-shading u-section-1" id="sec-bcac" data-image-width="1280" data-image-height="851">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <center><h2 id="h2idchhose" class="u-align-center u-text u-text-default u-text-1">Choose Charity Location (choose their origin country for international charities):<br><br><br></center>
+        </h2>
+        <div id="thisgodark" class="u-align-center u-clearfix u-custom-html u-custom-html-1">
+  <div id="choose-where-you-from">
+      
+      
+     <form style="display:none;" id="form1" method="POST" action="action.php">
+     <input id="fromCountry" name="fromCountry" type="text" />
+    <input id="sendbutton" type="submit" value="Send"/>
+</form>
+
+<!-- The result of the search will be rendered inside this div -->
+<div id="result" style="color:red;"></div>
+
+   <button class="choosewherefrom" onclick="country('nan')">
+      <span class=""></span> List All Charities
+    </button>
+ <br>
+    <br>
+
+
+
+    <button class="choosewherefrom" onclick="country('USA')">
+      <span class="fi fi-us"></span> U.S.A
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('Canada')">
+      <span class="fi fi-ca"></span> Canada
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('UK')">
+      <span class="fi fi-gb"></span> U.K.
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('EU')">
+      <span class="fi fi-eu"></span> European Union
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('turkey')">
+      <span class="fi fi-tr"></span> Turkey
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('azerbaijan')">
+      <span class="fi fi-az"></span> Azerbaijan
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('australia')">
+      <span class="fi fi-au"></span>Australia
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('newzeland')">
+      <span class="fi fi-nz"></span> New Zeland
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('sweden')">
+      <span class="fi fi-se"></span> Sweden
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('japan')">
+      <span class="fi fi-jp"></span> Japan
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('china')">
+      <span class="fi fi-cn"></span> China
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('korea')">
+      <span class="fi fi-kr"></span> Korea
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('finland')">
+      <span class="fi fi-fi"></span> Finland
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('israel')">
+      <span class="fi fi-il"></span> Israel
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('brazil')">
+      <span class="fi fi-br"></span> Brazil
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('chile')">
+      <span class="fi fi-cl"></span> Chile
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('argentina')">
+      <span class="fi fi-ar"></span> Argentina
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('uruguay')">
+      <span class="fi fi-uy"></span> Uruguay
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('syria')">
+      <span class="fi fi-sy"></span> Syria
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('iraq')">
+      <span class="fi fi-iq"></span> Iraq
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('lebenon')">
+      <span class="fi fi-lb"></span> Lebanon
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('georgia')">
+      <span class="fi fi-ge"></span> Georgia
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('jordan')">
+      <span class="fi fi-jo"></span> Jordan
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('uae')">
+      <span class="fi fi-ae"></span> U.A.E
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('qatar')">
+      <span class="fi fi-qa"></span> Qatar
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('saudi')">
+      <span class="fi fi-sa"></span> Saudi Arabia
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('singapore')">
+      <span class="fi fi-sg"></span> Singapore
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('thailand')">
+      <span class="fi fi-th"></span> Thailand
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('taiwan')">
+      <span class="fi fi-tw"></span> Taiwan
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('vietnam')">
+      <span class="fi fi-vn"></span> Vietnam
+    </button>
+    <br>
+    <br>
+    <button class="choosewherefrom" onclick="country('norway')">
+      <span class="fi fi-no"></span> Norway
+    </button>
+    <br>
+    <br>
+    <br>
+    <br>
+  </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-palette-1-dark-2 u-footer" id="sec-5c3f"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1"> Earthquake Helpers © / earthquakehelpers.com © - programmed by&nbsp;<a id="CRA-a" href="https://www.yunusemrevurgun.com/" class="u-active-none u-border-none u-btn u-button-style u-hover-none u-none u-text-palette-1-base u-btn-1">Yunus Emre Vurgun</a>
+        </p>
+      </div></footer>
+      
+     <?php
+     
+     
+     
+$server = "localhost";
+$user = " ";
+$pass = " ";
+$db = " ";
+
+
+ $conn = new mysqli($server, $user, $pass, $db);
+
+ if ($conn->connect_error) {
+  die();
+}
+ 
+if($chosenCNTRY != 'nan'){
+    
+
+$sql = "SELECT name, web, origin FROM turkey WHERE origin='$chosenCNTRY'";
+$result = $conn->query($sql);
+$arrayOfOrg=[];
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        
+        array_push($arrayOfOrg, $row['name'], $row['web'],$row['origin']);
+        
+    }
+} else {
+    echo "<script>alert('0 results for this country')</script>";
+}
+
+    
+} else{
+    
+         ///list all query below///////////////
+     
+     
+$sql2 = "SELECT name, web, origin FROM turkey";
+$result2 = $conn->query($sql2);
+if ($result2->num_rows > 0) {
+    // output data of each row
+    while($row2 = $result2->fetch_assoc()) {
+        
+        array_push($arrayOfOrg, $row2['name'], $row2['web'],$row2['origin']);
+        
+    }
+} else {
+    echo "<script>alert('0 results for this country')</script>";
+}
+     
+     
+     ///////////////////
+    
+}  
+    
+
+     if($chosenCNTRY=='nan'){
+         $chosenCNTRY='all around the world';
+     }
+
+
+       
+ echo "<script> let fromCountry = '".$chosenCNTRY."'; document.getElementById('h2idchhose').innerText='You can donate to Turkiye from ".$chosenCNTRY." via these organizations: '; document.getElementById('thisgodark').innerHTML='<br><br><table><tr> <th>Org. Name</th> <th>Website</th> <th>Origin</th> </tr></table>';";
+
+$size1 = sizeof($arrayOfOrg)/3;
+
+for($i=0;$i< $size1;$i++){
+    
+     echo "document.getElementById('thisgodark').innerHTML+=' <table><tr> <td>".$arrayOfOrg[0]."</td> <td><a href=\'".$arrayOfOrg[1]."\'>".$arrayOfOrg[1]."</a></td> <td>".$arrayOfOrg[2]."</td> </tr></table>';";
+     array_shift($arrayOfOrg); 
+     array_shift($arrayOfOrg);
+     array_shift($arrayOfOrg);
+}
+     
+
+ 
+ echo "</script>";
+ 
+ }
+     
+     ?> 
+     
+     
+    
+  </body>
+</html>
