0156750d30e3623a9d92bf2ffe918237ff4a9d59
YUNUS EMRE VURGUN <64273622+yunusemrejr@users.noreply.github.com>
2023-02-20 16:56:19 +0300

    Add files via upload

---
+++ syria/countryfunctions.js
@@ -0,0 +1,32 @@
+//to turkey from x
+
+
+
+
+function country(x){
+     document.getElementById('fromCountry').value=x;
+      
+     document.getElementById('sendbutton').click();
+
+    
+    
+    
+    
+
+}
+
+
+
+/*
+
+let replaceBody=document.getElementById('sec-bcac');
+    
+    replaceBody.innerHTML="<div id='dynamicDIV'><h3>You can donate to Syria from "+x+" via these organizations:</h3></div>";
+    
+    let getDIV =document.getElementById('dynamicDIV');
+    getDIV.style.paddingTop="80px";
+    getDIV.style.paddingBottom="50px";
+    getDIV.style.textAlign="center";
+
+
+*/
